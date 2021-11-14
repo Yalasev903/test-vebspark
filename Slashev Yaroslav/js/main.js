53aaf7ebf3c3b57adeefde52275d642f5ab93520
Yaroslav <slasev903@gmail.com>
2021-11-14 20:20:58 +0200

    commit last

--- Slashev Yaroslav/js/main.js
+++ Slashev Yaroslav/js/main.js
@@ -1,9 +1,14 @@
-const time = 5000;//ms
-const step = 117;
+const time = 7000;//ms
+const step = 1;
 
 function outNum(num, elem) {
      let l = document.querySelector('#' + elem)
      n = 0;
+     // if (step == 0) {
+     //      step = 1;
+     // }else{
+     //      step = step;
+     // }
      let t = Math.round(time / (num / step));
      let interval = setInterval(() => {
           n = n + step
@@ -15,4 +20,6 @@ l.innerHTML = n;
  t);
 }
 
-outNum(11700, 'out-num1');
+ outNum(11787, 'out-num1');
+// outNum(112, 'out-num3');
+ outNum(870, 'out-num2');
